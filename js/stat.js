960a9aa2a72237c15400649d4ba236269798ecdb
Moroschka <leda79@rambler.ru>
2020-09-22 10:53:01 +0300

    Список результатов

--- js/stat.js
+++ js/stat.js
@@ -5,10 +5,11 @@ const CLOUD_HEIGHT = 270;
 const CLOUD_X = 100;
 const CLOUD_Y = 10;
 const GAP = 10;
-const FONT_HEIGHT = 30;
+const FONT_GAP = 30;
 const BAR_GAP = 50;
 const BAR_WIDTH = 40;
 const MAX_BAR_HEIGHT = 150;
+
 
 const renderCloud = function(ctx, x, y, color) {
 	ctx.fillStyle = color;
@@ -31,27 +32,38 @@ window.renderStatistics = function(ctx, names, times) {
 	renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');	
 
 	ctx.fillStyle = '#000000';
-	const maxTime = getMaxElement(times);	
+	const maxTime = getMaxElement(times);
 
-	for (let i = 0; i < names.length; i++) {	
+	for (let i = 0; i < names.length; i++) {
+		if (names[i] === 'Вы') {
+			ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+		} else {			
+			const getRandomColor = function() {
+				let color = Math.floor(Math.random() * 100);
+				return 'hsl(240, 100%,' + color + '%' + ')';				
+			}
+			ctx.fillStyle = getRandomColor();
+		}
+
+		ctx.fillRect(
+			CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i,
+			CLOUD_HEIGHT - (times[i] * MAX_BAR_HEIGHT) / maxTime - FONT_GAP,
+			BAR_WIDTH,
+			(times[i] * MAX_BAR_HEIGHT) / maxTime);	
+
+		ctx.fillStyle = '#000000';
 		ctx.fillText(
 			names[i],
 			CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i,
 			CLOUD_HEIGHT - CLOUD_Y);
-		ctx.fillRect(
-			CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i,
-			CLOUD_HEIGHT - MAX_BAR_HEIGHT - FONT_HEIGHT,
-			BAR_WIDTH,
-			(times[i] * MAX_BAR_HEIGHT) / maxTime);
 		ctx.fillText(
 			Math.round(times[i]),
 			CLOUD_X + BAR_GAP + (BAR_WIDTH + BAR_GAP) * i,
-			80);
+			CLOUD_HEIGHT - CLOUD_Y - (times[i] * MAX_BAR_HEIGHT) / maxTime -  FONT_GAP);	 	
+
 	}
 
-	ctx.font = '16pt PT Mono';
-	ctx.fillStyle = '#000000';
+	ctx.font = '16px PT Mono';	
 	ctx.fillText('Ура вы победили!', 120, 40);
 	ctx.fillText('Список результатов:', 120, 60);
-
 };
