040e79d5c5eb76609f5660d76052b3b93bc18b5e
Moroschka <leda79@rambler.ru>
2020-09-22 16:28:27 +0300

    Домашняя работа на проверку

--- js/stat.js
+++ js/stat.js
@@ -9,6 +9,7 @@ const FONT_GAP = 30;
 const BAR_GAP = 50;
 const BAR_WIDTH = 40;
 const MAX_BAR_HEIGHT = 150;
+
 const renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
